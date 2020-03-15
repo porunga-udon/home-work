0abb6ac38924de53c8052cfd17f691b47b46a878
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-03-15 14:29:04 +0900

    login fuinction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
+  before_action :move_to_index , except: [:index]
+
   def index
+    @posts = Post.all
   end
+
+  def create
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
