0003a774c7dec482769aa0d4320ab56ba74908a6
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-03-15 17:59:20 +0900

    complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :move_to_index , except: [:index]
 
   def index
     @post = Post.new
@@ -15,10 +14,6 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
   end
 
 end
