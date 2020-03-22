6d0c7ce4da1724c41cbf7cd85694a6739da839bc
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-03-22 11:37:05 +0900

    post  Asynchronous communication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,19 @@ class PostsController < ApplicationController
     @post = Post.new
     @posts = Post.all
   end
-
+  
   def create
     @post = Post.create(post_params)
-    redirect_to root_path
+    if @post.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      render :index
+    end
   end
 
   private
-
   def post_params
     params.require(:post).permit(:content).merge(user_id: current_user.id)
   end
